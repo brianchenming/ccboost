94f2970a5d43ddd68d3a471896ba97ad1e34460e
Ming Chen <v.mingchen@gmail.com>
2013-08-31 18:03:32 -0400

    add init lib: gflags

---
+++ src/hello_world.cc
@@ -0,0 +1,17 @@
+/*
+ *      Author: Ming Chen, v.mingchen@gmail.com
+ * Description: ccboost Hello World.
+ */
+
+#include <gflags/gflags.h>
+#include <iostream>
+
+DEFINE_string(name, "World", "Name of the person of the greeting.");
+
+int main(int argc, char *argv[])
+{
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+  //google::ParseCommandLineFlags(&argc, &argv, true);
+  std::cout << "Hello, " << FLAGS_name << std::endl;
+  return 0;
+}
